64df7f4eca2d1c6fc89043b03f15c653122d1c1f
JEONGHWANMIN <mimi0j0j@naver.com>
2023-03-28 21:33:03 +0900

    Feat : 프로젝트 초기 구성

---
+++ mylogbe/src/main/java/com/example/mylogbe/MylogbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylogbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylogbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylogbeApplication.class, args);
+	}
+
+}
---
+++ mylogbe/src/test/java/com/example/mylogbe/MylogbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mylogbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylogbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
