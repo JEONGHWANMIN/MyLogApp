ce1b281c60fe7e3c01d10854d1ef39294af0204d
JEONGHWANMIN <mimi0j0j@naver.com>
2023-04-01 15:51:40 +0900

    Feat : AuthNavigation 타입 추가

--- app/src/navigation/AuthNavigation.tsx
+++ app/src/navigation/AuthNavigation.tsx
@@ -3,8 +3,9 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignIn from '@/screens/Auth/SignIn';
 import SignUp from '@/screens/Auth/SignUp';
 import Auth from '@/screens/Auth/Auth';
+import {AuthListParams} from './types/types';
 
-const AuthStack = createNativeStackNavigator();
+const AuthStack = createNativeStackNavigator<AuthListParams>();
 
 const AuthNavigation = () => {
   return (
