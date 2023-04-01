bc6d98c6ba111ab6a28e58b68dfb4a8351204aad
JEONGHWANMIN <mimi0j0j@naver.com>
2023-04-01 18:16:03 +0900

    Feat : theme 타입스크립트 설정 추가

--- app/src/styles/theme.ts
+++ app/src/styles/theme.ts
@@ -1,23 +1,52 @@
-import {MD3LightTheme as DefaultTheme} from 'react-native-paper';
-import {
-  MD2Colors,
-  MD3Colors,
-} from 'react-native-paper/lib/typescript/src/types';
+import {MD3LightTheme as DefaultTheme, useTheme} from 'react-native-paper';
+import {MD2Colors} from 'react-native-paper/lib/typescript/src/types';
 
 interface PointColor {
-  [key: string]: {
-    [key: string]: string;
+  point: {
+    sageGreen: '#99A98C';
+    olive: '#647445';
+    mintGreen: '#BED5C5';
+    taupe: '#B4AC9F';
+    parePink: '#FBD1DD';
+    dustBlue: '#A4B6C1';
+  };
+  gray: {
+    50: '#F7F8F9';
+    100: '#E8E8ED';
+    200: '#C9CDD2';
+    400: '#9EA4AA';
+    500: '#72787F';
+    600: '#454C53';
+    800: '#26292B';
+    900: '#1B1D1F';
   };
 }
 
 interface MyTheme {
-  colors: Partial<MD3Colors | MD2Colors> | PointColor;
+  colors: Partial<MD2Colors> & PointColor;
   typography: {
     size: {
-      [key: string]: number;
+      H1: 90;
+      H2: 54;
+      H3: 36;
+      H4: 28;
+      H5: 24;
+      H6: 20;
+      body1: 16;
+      body2: 15;
+      body3: 14;
+      body4: 13;
+      body5: 12;
+      body6: 11;
+      body7: 10;
+      body8: 9;
+      body9: 8;
     };
     weight: {
-      [key: string]: string;
+      regular: '100';
+      medium: '100';
+      bold: '100';
+      extraBold: '100';
     };
   };
 }
@@ -69,6 +98,10 @@ const theme: MyTheme = {
       extraBold: '100',
     },
   },
-};
+} as const;
 
-export {theme};
+export type AppTheme = typeof theme;
+
+const useAppTheme = () => useTheme<AppTheme>();
+
+export {theme, useAppTheme};
