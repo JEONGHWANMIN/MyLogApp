b102c008e7288492a1fd400334c77b54191e64b3
JEONGHWANMIN <mimi0j0j@naver.com>
2023-04-02 18:41:28 +0900

    Feat : Screen name 설정 및 headerTitle 적용

--- app/src/navigation/AuthNavigation.tsx
+++ app/src/navigation/AuthNavigation.tsx
@@ -3,9 +3,8 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignIn from '@/screens/Auth/SignIn';
 import SignUp from '@/screens/Auth/SignUp';
 import Auth from '@/screens/Auth/Auth';
-import {AuthListParams} from './types/types';
 
-const AuthStack = createNativeStackNavigator<AuthListParams>();
+const AuthStack = createNativeStackNavigator();
 
 const AuthNavigation = () => {
   return (
@@ -13,9 +12,27 @@ const AuthNavigation = () => {
       screenOptions={{
         headerTitleAlign: 'center',
       }}>
-      <AuthStack.Screen name="MyLog" component={Auth} />
-      <AuthStack.Screen name="로그인" component={SignIn} />
-      <AuthStack.Screen name="회원가입" component={SignUp} />
+      <AuthStack.Screen
+        name="Auth"
+        component={Auth}
+        options={{
+          headerTitle: 'PenPle',
+        }}
+      />
+      <AuthStack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{
+          headerTitle: '로그인',
+        }}
+      />
+      <AuthStack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{
+          headerTitle: '회원가입',
+        }}
+      />
     </AuthStack.Navigator>
   );
 };
