3d397d27716caf27de34b5d6452891db7a01b9f5
JEONGHWANMIN <mimi0j0j@naver.com>
2023-04-13 23:56:29 +0900

    Feat : 인풋 focus 추가

--- app/src/components/core/CoreInput.tsx
+++ app/src/components/core/CoreInput.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {theme} from '@/styles/theme';
 import {StyleSheet} from 'react-native';
 import {TextInput} from 'react-native-paper';
 import {Props} from 'react-native-paper/lib/typescript/src/components/TextInput/TextInput';
 
 const CoreInput = ({...props}: Props) => {
-  return <TextInput {...props} style={styles.container} />;
+  const [isFocused, setIsFocused] = useState(false);
+  return (
+    <TextInput
+      {...props}
+      style={[styles.container, isFocused && styles.focus]}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+    />
+  );
 };
 
 export default CoreInput;
 
 const styles = StyleSheet.create({
   container: {
-    borderColor: theme.colors.point.sageGreen,
+    borderColor: theme.colors.gray[200],
     borderWidth: 1,
     flex: 1,
     borderBottomEndRadius: 10,
@@ -20,5 +28,8 @@ const styles = StyleSheet.create({
     borderTopStartRadius: 10,
     borderTopEndRadius: 10,
     height: 50,
+  },
+  focus: {
+    borderColor: theme.colors.point.sageGreen,
   },
 });
