cf29364a5956f263f40de9f5c4e28db0eb212692
JEONGHWANMIN <mimi0j0j@naver.com>
2023-04-15 18:38:08 +0900

    Feat : 회원가입 페이지 레이아웃 추가

--- app/src/screens/Auth/SignUp.tsx
+++ app/src/screens/Auth/SignUp.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useNavigation} from '@react-navigation/native';
 import {theme} from '@/styles/theme';
 import {
   StyleSheet,
@@ -9,8 +10,10 @@ import {
 } from 'react-native';
 import CoreInput from '@/components/core/CoreInput';
 import CoreButton from '@/components/core/CoreButton';
+import {AuthParamListProps} from '@/navigation/types/types';
 
 const SignUp = () => {
+  const navigation = useNavigation<AuthParamListProps>();
   const handleCloseKeyboard = () => {
     Keyboard.dismiss();
   };
@@ -24,15 +27,31 @@ const SignUp = () => {
             {'을 통해 새로운 일상을 담아 \n일기처럼 기록해보세요.'}
           </Text>
           <View style={styles.formContainer}>
-            <View style={styles.duplicationView}>
-              <CoreInput placeholder="이메일" />
-              <CoreButton mode="contained">중복확인</CoreButton>
+            <View style={styles.rowFlexBox}>
+              <CoreInput
+                placeholder="이메일"
+                sx={{
+                  flex: 1,
+                }}
+              />
+              <CoreButton
+                mode="contained"
+                sx={{
+                  fontSize: 14,
+                }}>
+                중복확인
+              </CoreButton>
             </View>
-            <View style={styles.duplicationView}>
-              <CoreInput placeholder="패스워드" secureTextEntry />
-            </View>
-            <View style={styles.duplicationView}>
-              <CoreInput placeholder="패스워드 확인" secureTextEntry />
+            <CoreInput placeholder="닉네임" />
+            <CoreInput placeholder="패스워드" secureTextEntry />
+            <CoreInput placeholder="패스워드 확인" secureTextEntry />
+            <View style={styles.registeredUserContainer}>
+              <Text style={styles.registeredUserText}>이미 회원이신가요 ?</Text>
+              <Text
+                style={styles.loginLink}
+                onPress={() => navigation.push('SignIn')}>
+                로그인
+              </Text>
             </View>
           </View>
         </View>
@@ -79,12 +98,25 @@ const styles = StyleSheet.create({
     borderColor: theme.colors.gray[100],
     flex: 1,
   },
-  duplicationView: {
+  rowFlexBox: {
     flexDirection: 'row',
     gap: 5,
   },
   formContainer: {
     gap: 10,
     marginTop: 30,
+  },
+  registeredUserContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5,
+  },
+  registeredUserText: {
+    fontSize: theme.typography.size.body3,
+  },
+  loginLink: {
+    fontSize: theme.typography.size.body3,
+    fontWeight: theme.typography.weight.bold,
   },
 });
