66e7c80ec393fcdae072167740b83cb0301045e5
JEONGHWANMIN <mimi0j0j@naver.com>
2023-05-26 00:11:48 +0900

    Feat : validation pipe 설정 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe, ValidationPipeOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const validationOptions: ValidationPipeOptions = {
+  whitelist: true,
+  forbidNonWhitelisted: true,
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe(validationOptions));
   await app.listen(8080);
 }
 bootstrap();
