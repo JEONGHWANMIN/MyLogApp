c937b6f63beada4f3fe701ca5f970e29ff29cb0a
JEONGHWANMIN <mimi0j0j@naver.com>
2023-05-26 01:22:02 +0900

    Feat : createController 추가

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Injectable, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto';
+import { UsersService } from './users.service';
 
+@Injectable()
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   async createUser(@Body() createUserDto: CreateUserDto) {
-    console.log(createUserDto);
+    return this.usersService.create(createUserDto);
   }
 }
