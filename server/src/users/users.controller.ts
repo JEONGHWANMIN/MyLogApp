f8f26a3ab8135472ca4e9b54656f238302a9b5dc
JEONGHWANMIN <mimi0j0j@naver.com>
2023-05-28 21:24:03 +0900

    Feat : 유저 컨트롤러 추가

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Injectable, Post } from '@nestjs/common';
-import { CreateUserDto } from './dto';
+import {
+  Body,
+  Controller,
+  Get,
+  Injectable,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateUserDto, LoginUserDto } from './dto';
 import { UsersService } from './users.service';
+import { AccessTokenGuard } from '../common/guards/accessToken.guard';
+import { Request } from 'express';
+import { RefreshTokenGuard } from '../common/guards/refreshToken.guard';
 
 @Injectable()
 @Controller('users')
@@ -10,5 +21,24 @@ export class UsersController {
   @Post('/signup')
   async createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Post('/signin')
+  async loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.usersService.login(loginUserDto);
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Get('/logout')
+  logout(@Req() req: Request) {
+    return this.usersService.logout(req.user['userId']);
+  }
+
+  @UseGuards(RefreshTokenGuard)
+  @Get('/refresh')
+  refreshTokens(@Req() req: Request) {
+    const userId = req.user['userId'];
+    const refreshToken = req.user['refreshToken'];
+    return this.usersService.refreshTokens(userId, refreshToken);
   }
 }
