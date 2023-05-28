fc51a818f85292451728edf24dad69e97ca342c5
JEONGHWANMIN <ghksals09041@gmail.com>
2023-05-28 22:34:45 +0900

    Feat : custom decorator 추가

--- server/src/common/decorator/user.decorator.ts
+++ server/src/common/decorator/user.decorator.ts
@@ -3,10 +3,10 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const GetTokenUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    const { method, url, user } = request;
+    const { user } = request;
 
     if (user) return request.user;
-    console.log(`Method ${method} called for URL ${url}`);
+
     return request;
   },
 );
