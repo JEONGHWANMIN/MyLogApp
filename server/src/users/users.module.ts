38ac8d71020360e882fc423cc27990aa6e9552bb
JEONGHWANMIN <ghksals09041@gmail.com>
2023-05-28 22:28:01 +0900

    Feat : 경로 변경

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { AccessTokenStrategy } from './jwt/accessToken.strategy';
-import { RefreshTokenStrategy } from './jwt/refreshToken.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { AccessTokenStrategy, RefreshTokenStrategy } from './jwt';
 
 @Module({
   imports: [PrismaModule, JwtModule.register({})],
