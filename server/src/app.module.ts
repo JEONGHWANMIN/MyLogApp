e0ddab013dc8ff7dacd0fbeb8751851318000690
JEONGHWANMIN <ghksals09041@gmail.com>
2023-05-28 23:01:36 +0900

    Feat : Diary spec 반영

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigsModule } from './configs/configs.module';
 import { UsersModule } from './users/users.module';
+import { DiaryModule } from './diary/diary.module';
 
 @Module({
-  imports: [ConfigsModule, PrismaModule, UsersModule],
+  imports: [ConfigsModule, PrismaModule, UsersModule, DiaryModule],
   controllers: [],
   providers: [],
 })
