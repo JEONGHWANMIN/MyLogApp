ba6a9ab2023b6a50a182cb88d1ae4c77b482c859
JEONGHWANMIN <ghksals09041@gmail.com>
2023-05-29 00:17:54 +0900

    Feat: 다이어리 컨트롤러 추가

--- server/src/diary/diary.controller.ts
+++ server/src/diary/diary.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Injectable,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AccessTokenGuard } from 'src/common/guards';
+import { DiaryService } from './diary.service';
+import { GetTokenUser } from 'src/common/decorator/user.decorator';
+import { AccessTokenPayload } from 'src/users/types/TokenPayload.type';
+import { CreateDiaryDto } from './dto/diary.dto';
 
+@Injectable()
 @Controller('diary')
-export class DiaryController {}
+export class DiaryController {
+  constructor(private diaryService: DiaryService) {}
+
+  @UseGuards(AccessTokenGuard)
+  @Get('/')
+  async getAllDiaries() {
+    return await this.diaryService.getAllDiaries();
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Get('/:id')
+  async getDiaryById(
+    @GetTokenUser() user: AccessTokenPayload,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    const userId = user.userId;
+    return await this.diaryService.getDiaryById(userId, id);
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Post('/')
+  async createDiary(
+    @GetTokenUser() user: AccessTokenPayload,
+    @Body() createDiaryDto: CreateDiaryDto,
+  ) {
+    const userId = user.userId;
+    return await this.diaryService.createDiary(userId, createDiaryDto);
+  }
+
+  // TODO: 다이어리 수정하기
+
+  // TODO: 다이어리 삭제하기
+}
