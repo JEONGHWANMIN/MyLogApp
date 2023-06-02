f239a6e44802cca9ca4c631561d2e375a0a1f5b8
JEONGHWANMIN <mimi0j0j@naver.com>
2023-06-03 05:00:22 +0900

    Feat : 유저 로그인 스펙 추가

--- server/src/users/spec/user.spec.ts
+++ server/src/users/spec/user.spec.ts
@@ -1,39 +1,12 @@
 import { Tspec } from 'tspec';
 
-/** 도서 정보 */
-interface Book {
-  /** 도서 ID */
-  id: number;
-  /**
-   * 도서명
-   * @example 상수리 나무 아래
-   */
-  title: string;
-  tags: Tag[];
-}
-
-/** 태그 정보 */
-type Tag = '로맨스' | '판타지';
-
-export type BookApiSpec = Tspec.DefineApiSpec<{
-  tags: ['도서'];
+export type UsersApiSpec = Tspec.DefineApiSpec<{
+  basePath: 'users';
+  tags: ['users'];
   paths: {
-    '/books/{id}': {
-      get: {
-        summary: '단일 도서 조회';
-        path: { id: number };
-        responses: { 200: Book };
-      };
-    };
-  };
-}>;
-
-export type SignUpApiSpec = Tspec.DefineApiSpec<{
-  tags: ['유저'];
-  paths: {
-    '/users/signup': {
+    '/signup': {
       post: {
-        summary: '유저 회원 가입';
+        summary: '유저 회원가입';
         body: {
           email: string;
           nickname: string;
@@ -41,7 +14,23 @@ export type SignUpApiSpec = Tspec.DefineApiSpec<{
         };
         responses: {
           200: {
-            message: '사용자 생성에 성공했습니다.';
+            message: string;
+          };
+        };
+      };
+    };
+    '/login': {
+      post: {
+        summary: '유저 로그인';
+        body: {
+          email: string;
+          password: string;
+        };
+        responses: {
+          200: {
+            message: string;
+            accessToken: string;
+            refreshToken: string;
           };
         };
       };
