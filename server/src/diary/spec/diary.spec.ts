08cf71beab1d0c03d723ece088b7f14053dd45e8
JEONGHWANMIN <mimi0j0j@naver.com>
2023-06-04 20:22:46 +0900

    Feat : 일기 tspec 작성

---
+++ server/src/diary/spec/diary.spec.ts
@@ -0,0 +1,82 @@
+import { Diary } from '@prisma/client';
+import { Tspec } from 'tspec';
+import { CreateDiaryDto } from '../dto/diary.dto';
+
+export type DiaryApiSpec = Tspec.DefineApiSpec<{
+  basePath: 'diary';
+  tags: ['diary'];
+  paths: {
+    '/': {
+      get: {
+        summary: '일기 리스트 조회';
+        query: {
+          size: number;
+          page: number;
+        };
+        responses: {
+          200: {
+            message: string;
+            data: {
+              page: number;
+              size: number;
+              totalCount: number;
+              totalPage: number;
+              hasNextPage: boolean;
+              hasPreviousPage: boolean;
+              items: Diary[];
+            };
+          };
+        };
+      };
+      post: {
+        summary: '일기 쓰기';
+        body: CreateDiaryDto;
+        responses: {
+          201: {
+            message: string;
+          };
+        };
+      };
+    };
+    '/:id': {
+      get: {
+        summary: '일기 조회';
+        path: { id: number };
+        responses: {
+          200: {
+            message: string;
+            data: {
+              id: number;
+              title: string;
+              content: string;
+              createdAt: Date;
+              weather: string;
+              mood: string;
+              userId: number;
+              tags: string[];
+            };
+          };
+        };
+      };
+      patch: {
+        summary: '일기 수정';
+        path: { id: number };
+        body: CreateDiaryDto;
+        responses: {
+          200: {
+            message: string;
+          };
+        };
+      };
+      delete: {
+        summary: '일기 삭제';
+        path: { id: number };
+        responses: {
+          200: {
+            message: string;
+          };
+        };
+      };
+    };
+  };
+}>;
