917a7d9beb6a94ed76b2ab64da590abf6af26ccb
JEONGHWANMIN <mimi0j0j@naver.com>
2023-06-04 21:58:13 +0900

    Feat : orvalConfig custom instance 추가

--- app/orval.config.ts
+++ app/orval.config.ts
@@ -5,9 +5,15 @@ export default defineConfig({
     input: '../server/openapi.json',
     output: {
       mode: 'tags-split',
-      target: 'src/orval/target',
+      target: 'src/orval/api',
       schemas: 'src/orval/model',
       client: 'react-query',
+      override: {
+        mutator: {
+          path: './src/api/Instance.ts',
+          name: 'customInstance',
+        },
+      },
     },
     hooks: {
       afterAllFilesWrite: 'prettier --write',
