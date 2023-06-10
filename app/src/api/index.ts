623b4708981071a6be18f5b5bfebbb132dd59b8b
JEONGHWANMIN <mimi0j0j@naver.com>
2023-06-10 23:19:54 +0900

    Feat : config 옵션 추가

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -4,10 +4,10 @@ import axios, {AxiosRequestConfig} from 'axios';
 const _ANDROID_AVD_API_HOST = 'http://10.0.2.2:8080';
 const _IOS_API_HOST = 'http://localhost:8080';
 
-const config: AxiosRequestConfig = {
+const customConfig: AxiosRequestConfig = {
   baseURL: Platform.OS === 'android' ? _ANDROID_AVD_API_HOST : _IOS_API_HOST,
 };
 
-const customAxiosInstance = axios.create(config);
+const customAxiosInstance = axios.create(customConfig);
 
 export {customAxiosInstance};
