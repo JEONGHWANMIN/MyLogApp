60bad774041c02bd552864a73d85bc22d81ac7e2
JEONGHWANMIN <mimi0j0j@naver.com>
2023-06-15 23:49:40 +0900

    Feat : RootNavigation 로그인 조건 삭제

--- app/src/navigation/RootNavigation.tsx
+++ app/src/navigation/RootNavigation.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BottomTabNavigation from './BottomTabNavigation';
 import AuthNavigation from './AuthNavigation';
@@ -6,21 +6,16 @@ import AuthNavigation from './AuthNavigation';
 const RootStack = createNativeStackNavigator();
 
 const RootNavigation = () => {
-  const [isLogin] = useState(false);
-
   return (
     <RootStack.Navigator
       screenOptions={{
         headerShown: false,
       }}>
-      {isLogin ? (
-        <RootStack.Screen
-          name="BottomTabNavigation"
-          component={BottomTabNavigation}
-        />
-      ) : (
-        <RootStack.Screen name="AuthNavigation" component={AuthNavigation} />
-      )}
+      <RootStack.Screen name="AuthNavigation" component={AuthNavigation} />
+      <RootStack.Screen
+        name="BottomTabNavigation"
+        component={BottomTabNavigation}
+      />
     </RootStack.Navigator>
   );
 };
