65168b6ed218993e659aca8aa822f9344177cdbe
JEONGHWANMIN <mimi0j0j@naver.com>
2023-06-17 23:30:20 +0900

    Feat : 로그인 체크 로직 추가

--- app/src/navigation/RootNavigation.tsx
+++ app/src/navigation/RootNavigation.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BottomTabNavigation from './BottomTabNavigation';
 import AuthNavigation from './AuthNavigation';
+import {LocalStorage} from '@/utils/localStorage/localStorage';
 
 const RootStack = createNativeStackNavigator();
 
 const RootNavigation = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    const checkLogin = async () => {
+      const localStorage = LocalStorage.getInstance();
+
+      const accessToken = await localStorage.getData('accessToken');
+
+      if (accessToken) {
+        setIsLogin(true);
+      }
+    };
+  }, []);
   return (
     <RootStack.Navigator
       screenOptions={{
