8aa648880c49f54861805b7d2e5d24bff619a11e
JEONGHWANMIN <mimi0j0j@naver.com>
2023-06-18 16:59:42 +0900

    Feat : 초기 스플래쉬 스크린 추가

--- app/src/navigation/RootNavigation.tsx
+++ app/src/navigation/RootNavigation.tsx
@@ -1,29 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BottomTabNavigation from './BottomTabNavigation';
 import AuthNavigation from './AuthNavigation';
-import {LocalStorage} from '@/utils/localStorage/localStorage';
+
+import SplashScreen from '@/screens/SplashScreen/SplashScreen';
 
 const RootStack = createNativeStackNavigator();
 
 const RootNavigation = () => {
-  const [isLogin, setIsLogin] = useState(false);
-  useEffect(() => {
-    const checkLogin = async () => {
-      const localStorage = LocalStorage.getInstance();
-
-      const accessToken = await localStorage.getData('accessToken');
-
-      if (accessToken) {
-        setIsLogin(true);
-      }
-    };
-  }, []);
   return (
     <RootStack.Navigator
+      initialRouteName="SplashScreen"
       screenOptions={{
         headerShown: false,
       }}>
+      <RootStack.Screen name="SplashScreen" component={SplashScreen} />
       <RootStack.Screen name="AuthNavigation" component={AuthNavigation} />
       <RootStack.Screen
         name="BottomTabNavigation"
