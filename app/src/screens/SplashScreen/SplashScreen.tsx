14792d67b78c7f55b5a5f4b230c987f0c7390913
JEONGHWANMIN <mimi0j0j@naver.com>
2023-06-18 17:00:18 +0900

    Feat : 스플래쉬 스크린 추가

---
+++ app/src/screens/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,59 @@
+import React, {useEffect} from 'react';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {LocalStorage} from '@/utils/localStorage/localStorage';
+import {useNavigation} from '@react-navigation/native';
+import {
+  AuthParamListProps,
+  RootListParamsListProps,
+} from '@/navigation/types/types';
+import {theme} from '@/styles/theme';
+
+const SplashScreen = () => {
+  const navigation = useNavigation<
+    AuthParamListProps & RootListParamsListProps
+  >();
+  useEffect(() => {
+    const checkLogin = async () => {
+      const localStorage = LocalStorage.getInstance();
+
+      const accessToken = await localStorage.getData('accessToken');
+
+      if (accessToken) {
+        return navigation.replace('BottomTabNavigation');
+      }
+
+      navigation.replace('AuthNavigation');
+    };
+
+    const timer = setTimeout(() => {
+      checkLogin();
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.splashText}>PenPle</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: theme.colors.point.mintGreen,
+  },
+  splashText: {
+    color: 'white',
+    fontSize: theme.typography.size.H3,
+    fontWeight: theme.typography.weight.extraBold,
+    letterSpacing: 2.5,
+  },
+});
