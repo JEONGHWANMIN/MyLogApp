81b324da545b5a5dc6553716e9bd4f6aea9e4c07
JEONGHWANMIN <mimi0j0j@naver.com>
2023-06-22 23:52:45 +0900

    Feat : eslint prettier 옵션 끔

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -2,11 +2,6 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
+    'prettier/prettier': 'off',
   },
 };
