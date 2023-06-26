01621878243f19c0b561f8880d1f6704c683fa8e
JEONGHWANMIN <mimi0j0j@naver.com>
2023-06-26 23:45:58 +0900

    Refactor : 리액트 훅 폼 사용

--- app/src/screens/Auth/SignUp.tsx
+++ app/src/screens/Auth/SignUp.tsx
@@ -2,39 +2,38 @@ import React, {useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import axios from 'axios';
 import {theme} from '@/styles/theme';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Keyboard,
-  TouchableWithoutFeedback,
-} from 'react-native';
+import {StyleSheet, Text, View, Keyboard, TouchableWithoutFeedback} from 'react-native';
 import CoreInput from '@/components/core/CoreInput';
 import CoreButton from '@/components/core/CoreButton';
 import {AuthParamListProps} from '@/navigation/types/types';
-import {
-  useUsersApiSpecPostUsersSignup,
-  usersApiSpecGetUsersCheck,
-} from '@/orval/api/users/users';
+import {useUsersApiSpecPostUsersSignup, usersApiSpecGetUsersCheck} from '@/orval/api/users/users';
 import {UsersApiSpecPostUsersSignupBody} from '@/orval/model';
 import {useGlobalSnackbarStore} from '@/utils/state/snackbar.zustand';
+import {Controller, useForm} from 'react-hook-form';
+
+interface SignUpForm {
+  email: string;
+  nickname: string;
+  password: string;
+  passwordMatch: string;
+}
 
 const SignUp = () => {
   const navigation = useNavigation<AuthParamListProps>();
   const {setGlobalSnackbar} = useGlobalSnackbarStore();
-  const [passwordMatch, setPasswordMatch] = useState(true);
-  const [isEmailDuplicate, setIsEmailDuplicate] = useState(true);
-  const [form, setForm] = useState({
-    email: '',
-    nickname: '',
-    password: '',
-    confirmPassword: '',
+
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<SignUpForm>({
+    defaultValues: {
+      email: '',
+      nickname: '',
+      password: '',
+      passwordMatch: '',
+    },
   });
-
-  const isEmptyField =
-    !form.email || !form.nickname || !form.password || !form.confirmPassword;
-
-  const useUsersApiSpecPostUsers = useUsersApiSpecPostUsersSignup();
 
   const handleCloseKeyboard = () => {
     Keyboard.dismiss();
@@ -50,46 +49,9 @@ const SignUp = () => {
       },
     });
   };
+  const useUsersApiSpecPostUsers = useUsersApiSpecPostUsersSignup();
 
-  const handleConfirmPasswordCheck = () => {
-    if (form.password) {
-      setPasswordMatch(form.password === form.confirmPassword);
-    }
-    if (!form.password) {
-      setPasswordMatch(true);
-    }
-  };
-
-  const onChange = (text: string, name: string) => {
-    if (name === 'confirmPassword') {
-      setPasswordMatch(form.password === text);
-    }
-
-    setForm(prevForm => ({
-      ...prevForm,
-      [name]: text,
-    }));
-  };
-
-  const handleEmailDuplicate = async () => {
-    const {isDuplicate} = await usersApiSpecGetUsersCheck({
-      email: form.email,
-    });
-
-    const message = isDuplicate
-      ? '중복된 이메일 입니다.'
-      : '사용가능한 이메일 입니다.';
-
-    const mode = isDuplicate ? 'error' : 'info';
-
-    showSnackbarMessage(message, mode);
-
-    setIsEmailDuplicate(isDuplicate);
-  };
-
-  const usersApiSpecPostUsers = (
-    signUpForm: UsersApiSpecPostUsersSignupBody,
-  ) => {
+  const usersApiSpecPostUsers = (signUpForm: UsersApiSpecPostUsersSignupBody) => {
     useUsersApiSpecPostUsers.mutate(
       {
         data: signUpForm,
@@ -108,16 +70,11 @@ const SignUp = () => {
     );
   };
 
-  const handleSubmit = async () => {
-    if (!passwordMatch) {
-      showSnackbarMessage('비밀번호가 일치하지 않습니다.', 'error');
-      return;
-    }
-
+  const onSubmit = (data: SignUpForm) => {
     const requestForm: UsersApiSpecPostUsersSignupBody = {
-      email: form.email,
-      nickname: form.nickname,
-      password: form.password,
+      email: data.email,
+      nickname: data.nickname,
+      password: data.password,
     };
 
     usersApiSpecPostUsers(requestForm);
@@ -134,66 +91,88 @@ const SignUp = () => {
           </Text>
           <View style={styles.formContainer}>
             <View style={styles.rowFlexBox}>
-              <CoreInput
-                value={form.email}
-                placeholder="이메일"
-                onChangeText={text => onChange(text, 'email')}
-                sx={{
-                  flex: 1,
+              <Controller
+                control={control}
+                rules={{
+                  required: true,
                 }}
+                render={({field: {onChange, onBlur, value}}) => (
+                  <CoreInput
+                    placeholder="email"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    sx={{
+                      flex: 1,
+                    }}
+                  />
+                )}
+                name="email"
               />
               <CoreButton
                 mode="contained"
-                onPress={handleEmailDuplicate}
+                // onPress={handleEmailDuplicate}
                 sx={{
                   fontSize: 14,
                 }}>
                 중복확인
               </CoreButton>
             </View>
-            <CoreInput
-              placeholder="닉네임"
-              value={form.nickname}
-              onChangeText={text => onChange(text, 'nickname')}
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <CoreInput
+                  placeholder="First name"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="nickname"
             />
-            <CoreInput
-              placeholder="패스워드"
-              value={form.password}
-              onChangeText={text => onChange(text, 'password')}
-              secureTextEntry
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <CoreInput
+                  placeholder="First name"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="password"
             />
-            <CoreInput
-              placeholder="패스워드 확인"
-              value={form.confirmPassword}
-              onChangeText={text => onChange(text, 'confirmPassword')}
-              secureTextEntry
-              error={!passwordMatch}
-              onBlurEvent={handleConfirmPasswordCheck}
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <CoreInput
+                  placeholder="First name"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="passwordMatch"
             />
             <View style={styles.registeredUserContainer}>
               <Text style={styles.registeredUserText}>이미 회원이신가요 ?</Text>
-              <Text
-                style={styles.loginLink}
-                onPress={() => navigation.push('SignIn')}>
+              <Text style={styles.loginLink} onPress={() => navigation.push('SignIn')}>
                 로그인
               </Text>
             </View>
-            {!passwordMatch && (
-              <Text style={styles.errorMessage}>
-                비밀번호가 일치하지 않습니다.
-              </Text>
-            )}
           </View>
         </View>
         <View>
-          <CoreButton
-            mode="contained"
-            onPress={handleSubmit}
-            disabled={
-              useUsersApiSpecPostUsers.isLoading ||
-              isEmailDuplicate ||
-              isEmptyField
-            }>
+          <CoreButton mode="contained" onPress={handleSubmit(onSubmit)}>
             회원가입
           </CoreButton>
         </View>
