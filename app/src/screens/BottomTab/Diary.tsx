34c1b55f58bcdaeac6554e2122c55a3220974633
JEONGHWANMIN <mimi0j0j@naver.com>
2023-07-07 01:38:19 +0900

    Feat : 글쓰기 버튼 플로팅 추가

--- app/src/screens/BottomTab/Diary.tsx
+++ app/src/screens/BottomTab/Diary.tsx
@@ -1,12 +1,30 @@
+import {DiaryWriteButton} from '@/components/core/DiaryWriteButton';
+import {theme} from '@/styles/theme';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 const Diary = () => {
   return (
-    <View>
-      <Text>다이어리</Text>
+    <View style={styles.container}>
+      <DiaryWriteButton />
     </View>
   );
 };
 
 export default Diary;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  writeDiaryButton: {
+    position: 'absolute',
+    bottom: 30,
+    right: 30,
+    backgroundColor: theme.colors.point.mintGreen,
+    borderRadius: 50,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
