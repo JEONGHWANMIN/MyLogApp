6357d9ae550e9bd3520581e2a1c3357cabbff808
JEONGHWANMIN <mimi0j0j@naver.com>
2023-07-09 22:06:52 +0900

    Feat : openapi docs 컨트롤러 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigsModule } from './configs/configs.module';
 import { UsersModule } from './users/users.module';
 import { DiaryModule } from './diary/diary.module';
+import { DocsModule } from './docs/docs.module';
 
 @Module({
-  imports: [ConfigsModule, PrismaModule, UsersModule, DiaryModule],
+  imports: [ConfigsModule, PrismaModule, UsersModule, DiaryModule, DocsModule],
   controllers: [],
   providers: [],
 })
