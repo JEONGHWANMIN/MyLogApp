453c39184358296b70e37b6b50039cd0829fa6f9
JEONGHWANMIN <mimi0j0j@naver.com>
2023-07-11 01:20:49 +0900

    Feat : API_URL ENV 변경

--- app/babel.config.js
+++ app/babel.config.js
@@ -7,6 +7,17 @@ module.exports = {
   },
   plugins: [
     [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+    [
       'module-resolver',
       {
         root: ['./src'],
--- app/src/api/Instance.ts
+++ app/src/api/Instance.ts
@@ -1,17 +1,20 @@
-import {Platform} from 'react-native';
 import axios, {AxiosError, AxiosRequestConfig, AxiosRequestHeaders, AxiosResponse} from 'axios';
+import {API_URL} from '@env';
+import {Platform} from 'react-native';
 import {LocalStorage} from '../utils/localStorage/localStorage';
 import {usersApiSpecGetUsersRenew} from '@/orval/api/users/users';
 export type ErrorType<Error> = AxiosError<Error>;
 
+// Local Test
 // const _ANDROID_AVD_API_HOST = 'http://10.0.2.2:8080';
 // const _IOS_API_HOST = 'http://localhost:8080';
 
-const _ANDROID_AVD_API_HOST = 'http://192.168.1.7:8080';
-const _IOS_API_HOST = 'http://192.168.1.7:8080';
+// Local WIFI Test
+// const _ANDROID_AVD_API_HOST = 'http://192.168.1.7:8080';
+// const _IOS_API_HOST = 'http://192.168.1.7:8080';
 
 const customConfig: AxiosRequestConfig = {
-  baseURL: Platform.OS === 'android' ? _ANDROID_AVD_API_HOST : _IOS_API_HOST,
+  baseURL: Platform.OS === 'android' ? API_URL : API_URL,
 };
 
 export const customAxiosInstance = axios.create(customConfig);
