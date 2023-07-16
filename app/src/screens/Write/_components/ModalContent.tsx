ac58470af2f176cd5f7feacc0eb265772f3d83ed
JEONGHWANMIN <mimi0j0j@naver.com>
2023-07-16 23:22:28 +0900

    Feat : 일기 작성 모달 컴포넌트 레이아웃 완료

--- app/src/screens/Write/_components/ModalContent.tsx
+++ app/src/screens/Write/_components/ModalContent.tsx
@@ -3,47 +3,19 @@ import React, {useState} from 'react';
 import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {IconButton} from 'react-native-paper';
 import {MOODS, WEATHERS} from '../_constants/_constants';
+import {Option} from '../Write';
 
-const WeatherSetting = () => {
-  return (
-    <FlatList
-      data={WEATHERS}
-      renderItem={({item}) => (
-        <View style={styles.iconContainer}>
-          <TouchableOpacity style={styles.iconContainer} onPress={() => console.log('asd')}>
-            <IconButton icon={item.key} size={40} iconColor={item.color} />
-            <Text>{item.description}</Text>
-          </TouchableOpacity>
-        </View>
-      )}
-      numColumns={3}
-      keyExtractor={item => item.key}
-    />
-  );
-};
+export type TAB_NAME = '기분' | '날씨';
 
-const MoodSetting = () => {
-  return (
-    <FlatList
-      data={MOODS}
-      renderItem={({item}) => (
-        <View style={styles.iconContainer}>
-          <TouchableOpacity style={styles.iconContainer} onPress={() => console.log('asd')}>
-            <IconButton icon={item.key} size={40} iconColor={item.color} />
-            <Text>{item.description}</Text>
-          </TouchableOpacity>
-        </View>
-      )}
-      numColumns={3}
-      keyExtractor={item => item.key}
-    />
-  );
-};
+interface ModalContentProps {
+  defaultTab?: TAB_NAME;
+  handleChangeOptions: (name: string, option: Option) => void;
+}
 
-const ModalContent = () => {
-  const [tab, setTab] = useState('날씨');
+const ModalContent = ({handleChangeOptions, defaultTab = '날씨'}: ModalContentProps) => {
+  const [tab, setTab] = useState<TAB_NAME>(defaultTab);
 
-  const handleChangeTab = (tabName: string) => {
+  const handleChangeTab = (tabName: TAB_NAME) => {
     setTab(tabName);
   };
 
@@ -53,14 +25,17 @@ const ModalContent = () => {
         <Text style={styles.title}>오늘의 하루를 아이콘으로 표현해보세요!</Text>
       </View>
       <View style={styles.tabContainer}>
-        <TouchableOpacity onPress={() => handleChangeTab('날씨')} style={styles.tab}>
-          <Text style={styles.tabText}>날씨</Text>
-        </TouchableOpacity>
         <TouchableOpacity onPress={() => handleChangeTab('기분')} style={styles.tab}>
-          <Text style={styles.tabText}>기분</Text>
+          <Text style={[styles.tabText, tab === '기분' && styles.activeTabText]}>기분</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleChangeTab('날씨')} style={styles.tab}>
+          <Text style={[styles.tabText, tab === '날씨' && styles.activeTabText]}>날씨</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.borderTob}>{tab === '날씨' ? <WeatherSetting /> : <MoodSetting />}</View>
+      <View style={styles.borderTob}>
+        {tab === '기분' && <MoodSetting handleChangeOptions={handleChangeOptions} />}
+        {tab === '날씨' && <WeatherSetting handleChangeOptions={handleChangeOptions} />}
+      </View>
     </View>
   );
 };
@@ -94,6 +69,9 @@ const styles = StyleSheet.create({
     fontWeight: theme.typography.weight.bold,
     fontSize: 14,
   },
+  activeTabText: {
+    color: theme.colors.gray[800],
+  },
   borderTob: {
     // borderTopWidth: 1,
     // borderTopColor: 'red',
@@ -103,5 +81,48 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  icon: {},
 });
+
+interface WeatherSettingProps extends ModalContentProps {}
+
+const WeatherSetting = ({handleChangeOptions}: WeatherSettingProps) => {
+  return (
+    <FlatList
+      data={WEATHERS}
+      renderItem={({item}) => (
+        <View style={styles.iconContainer}>
+          <TouchableOpacity
+            style={styles.iconContainer}
+            onPress={() => handleChangeOptions('weather', item)}>
+            <IconButton icon={item.key} size={40} iconColor={item.color} />
+            <Text>{item.description}</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      numColumns={3}
+      keyExtractor={item => item.key}
+    />
+  );
+};
+
+interface MoodSettingProps extends ModalContentProps {}
+
+const MoodSetting = ({handleChangeOptions}: MoodSettingProps) => {
+  return (
+    <FlatList
+      data={MOODS}
+      renderItem={({item}) => (
+        <View style={styles.iconContainer}>
+          <TouchableOpacity
+            style={styles.iconContainer}
+            onPress={() => handleChangeOptions('mood', item)}>
+            <IconButton icon={item.key} size={40} iconColor={item.color} />
+            <Text>{item.description}</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      numColumns={3}
+      keyExtractor={item => item.key}
+    />
+  );
+};
