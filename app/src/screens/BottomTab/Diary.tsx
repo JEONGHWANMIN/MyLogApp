181eacef70f8608bc15649214562d393be1cc8a7
JEONGHWANMIN <mimi0j0j@naver.com>
2023-07-18 23:49:49 +0900

    Feat : 다이어리 리스트 조회 API 연결

--- app/src/screens/BottomTab/Diary.tsx
+++ app/src/screens/BottomTab/Diary.tsx
@@ -1,11 +1,21 @@
 import {DiaryWriteButton} from '@/components/core/DiaryWriteButton';
-import {theme} from '@/styles/theme';
+import {useDiaryApiSpecGetDiary} from '@/orval/api/diary/diary';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import {Text} from 'react-native-paper';
 
 const Diary = () => {
+  const diaryListStatus = useDiaryApiSpecGetDiary({
+    page: 1,
+    size: 10,
+  });
+
+  console.log(diaryListStatus.data?.data.items);
+
+  const diaryList = diaryListStatus.data?.data.items;
   return (
     <View style={styles.container}>
+      <Text>{JSON.stringify(diaryList)}</Text>
       <DiaryWriteButton />
     </View>
   );
@@ -17,14 +27,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  writeDiaryButton: {
-    position: 'absolute',
-    bottom: 30,
-    right: 30,
-    backgroundColor: theme.colors.point.mintGreen,
-    borderRadius: 50,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // writeDiaryButton: {
+  //   position: 'absolute',
+  //   bottom: 30,
+  //   right: 30,
+  //   backgroundColor: theme.colors.point.mintGreen,
+  //   borderRadius: 50,
+  //   display: 'flex',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
 });
