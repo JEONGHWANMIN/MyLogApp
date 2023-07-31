0ef1d29f6996fdcc44aee37f66f140712f3a0fe8
JEONGHWANMIN <mimi0j0j@naver.com>
2023-07-31 22:45:53 +0900

    Feat : FlatList API Data 연결

--- app/src/screens/BottomTab/Diary/Diary.tsx
+++ app/src/screens/BottomTab/Diary/Diary.tsx
@@ -1,21 +1,28 @@
 import {DiaryWriteButton} from '@/components/core/DiaryWriteButton';
 import {useDiaryApiSpecGetDiary} from '@/orval/api/diary/diary';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {FlatList, StyleSheet, View} from 'react-native';
 import {DiaryItem} from './_components/DiaryItem';
 
 const Diary = () => {
-  // const diaryListStatus = useDiaryApiSpecGetDiary({
-  //   page: 1,
-  //   size: 10,
-  // });
+  const diaryListStatus = useDiaryApiSpecGetDiary({
+    page: 1,
+    size: 10,
+  });
 
-  // const diaryList = diaryListStatus.data?.data.items;
+  const diaryList = diaryListStatus.data?.data.items;
+
+  console.log(diaryList);
+  const ItemSeparator = () => <View style={styles.itemSeparator} />;
 
   return (
     <View style={styles.container}>
       <View style={styles.diaryListView}>
-        <DiaryItem />
+        <FlatList
+          data={diaryList}
+          renderItem={({item}) => <DiaryItem diaryItem={item} />}
+          ItemSeparatorComponent={ItemSeparator}
+        />
       </View>
       <DiaryWriteButton />
     </View>
@@ -30,5 +37,8 @@ const styles = StyleSheet.create({
   },
   diaryListView: {
     paddingHorizontal: 12,
+  },
+  itemSeparator: {
+    height: 10, // Set the desired spacing between items here
   },
 });
