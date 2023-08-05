e7f4c63747b10a2038ffaccb8eca88306b0acd8b
JEONGHWANMIN <mimi0j0j@naver.com>
2023-08-05 23:44:01 +0900

    Feat : 배경 색 변경 및 SafeAreaView 생성

--- app/src/screens/BottomTab/Diary/Diary.tsx
+++ app/src/screens/BottomTab/Diary/Diary.tsx
@@ -1,14 +1,26 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {DiaryWriteButton} from '@/components/core/DiaryWriteButton';
 import {diaryApiSpecGetDiary} from '@/orval/api/diary/diary';
-import React from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {
+  FlatList,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {DiaryItem} from './_components/DiaryItem';
 import {useInfiniteQuery} from '@tanstack/react-query';
 import {ActivityIndicator} from 'react-native-paper';
+import {theme} from '@/styles/theme';
 
 const ItemSeparator = () => <View style={styles.itemSeparator} />;
 
 const Diary = () => {
+  const flatListRef = useRef(null);
+
   const diaryListStatus = useInfiniteQuery(['diaryList'], {
     queryFn: ({pageParam = 1}) => diaryApiSpecGetDiary({page: pageParam, size: 6}),
     getNextPageParam: lastPage => {
@@ -16,40 +28,86 @@ const Diary = () => {
     },
   });
 
+  useEffect(() => {
+    if (diaryListStatus.status === 'success') {
+      console.log(diaryListStatus.data.pages[0]);
+    }
+
+    if (diaryListStatus.status === 'error') {
+      console.log(diaryListStatus.error);
+    }
+  }, [diaryListStatus.status]);
+
+  const diaryList = diaryListStatus.data?.pages.flatMap(page => page.data.items) || [];
+
   const handleLoadMore = () => {
     if (diaryListStatus.hasNextPage) {
       diaryListStatus.fetchNextPage();
     }
   };
 
-  const diaryList = diaryListStatus.data?.pages.flatMap(page => page.data.items) || [];
+  const prefetchNextPage = () => {
+    if (diaryListStatus.hasNextPage && !diaryListStatus.isFetchingNextPage) {
+      const nextPage = (diaryListStatus.data?.pages.at(-1)?.data.page ?? 0) + 1;
+      diaryListStatus.fetchNextPage({pageParam: nextPage + 1});
+    }
+  };
+
+  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const {contentOffset, layoutMeasurement, contentSize} = event.nativeEvent;
+    const paddingToBottom = 60;
+    if (contentOffset.y + layoutMeasurement.height >= contentSize.height - paddingToBottom) {
+      prefetchNextPage();
+    }
+  };
+
+  console.log(diaryList);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.diaryListView}>
-        <FlatList
-          data={diaryList}
-          renderItem={({item}) => <DiaryItem diaryItem={item} />}
-          ItemSeparatorComponent={ItemSeparator}
-          onEndReached={handleLoadMore}
-          onEndReachedThreshold={0.1}
-          ListFooterComponent={diaryListStatus.isFetchingNextPage ? <ActivityIndicator /> : null}
-        />
+    <SafeAreaView style={styles.safeAreaContainer}>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.dateText}>2023-08월</Text>
+        </View>
+        <View style={styles.diaryListView}>
+          <FlatList
+            ref={flatListRef}
+            data={diaryList}
+            renderItem={({item}) => <DiaryItem diaryItem={item} />}
+            ItemSeparatorComponent={ItemSeparator}
+            onEndReached={handleLoadMore}
+            onEndReachedThreshold={0.1}
+            ListFooterComponent={diaryListStatus.isFetchingNextPage ? <ActivityIndicator /> : null}
+            onScroll={handleScroll}
+            scrollEventThrottle={400}
+          />
+        </View>
+        <DiaryWriteButton />
       </View>
-      <DiaryWriteButton />
-    </View>
+    </SafeAreaView>
   );
 };
 
 export default Diary;
 
 const styles = StyleSheet.create({
-  container: {
+  safeAreaContainer: {
     flex: 1,
   },
-  diaryListView: {
-    // flex: 1,
+  container: {
+    flex: 1,
     paddingHorizontal: 12,
+    backgroundColor: 'white',
+  },
+  diaryListView: {
+    flex: 1,
+    marginBottom: 20,
+  },
+  dateText: {
+    marginVertical: 12,
+    color: theme.colors.gray[800],
+    fontFamily: theme.typography.family.semiBold,
+    fontSize: theme.typography.size.H5,
   },
   itemSeparator: {
     height: 10, // Set the desired spacing between items here
