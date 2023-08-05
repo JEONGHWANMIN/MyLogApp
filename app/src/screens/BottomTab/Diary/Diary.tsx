4e99f4097ae8c6ecc6a6274ffd3aad8af763a9f4
JEONGHWANMIN <mimi0j0j@naver.com>
2023-08-06 02:12:04 +0900

    Feat : MonthPicker 추가

--- app/src/screens/BottomTab/Diary/Diary.tsx
+++ app/src/screens/BottomTab/Diary/Diary.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import {DiaryWriteButton} from '@/components/core/DiaryWriteButton';
 import {diaryApiSpecGetDiary} from '@/orval/api/diary/diary';
-import React, {useEffect, useRef} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {
   FlatList,
   NativeScrollEvent,
   NativeSyntheticEvent,
+  Pressable,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -15,11 +16,28 @@ import {DiaryItem} from './_components/DiaryItem';
 import {useInfiniteQuery} from '@tanstack/react-query';
 import {ActivityIndicator} from 'react-native-paper';
 import {theme} from '@/styles/theme';
+import MonthPicker from 'react-native-month-year-picker';
 
 const ItemSeparator = () => <View style={styles.itemSeparator} />;
 
+// https://www.npmjs.com/package/react-native-month-year-picker
 const Diary = () => {
   const flatListRef = useRef(null);
+
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  // const showPicker = useCallback(value => setShow(value), []);
+
+  // const onValueChange = useCallback(
+  //   (event, newDate) => {
+  //     const selectedDate = newDate || date;
+
+  //     showPicker(false);
+  //     setDate(selectedDate);
+  //   },
+  //   [date, showPicker],
+  // );
 
   const diaryListStatus = useInfiniteQuery(['diaryList'], {
     queryFn: ({pageParam = 1}) => diaryApiSpecGetDiary({page: pageParam, size: 6}),
@@ -61,14 +79,12 @@ const Diary = () => {
     }
   };
 
-  console.log(diaryList);
-
   return (
     <SafeAreaView style={styles.safeAreaContainer}>
       <View style={styles.container}>
-        <View>
-          <Text style={styles.dateText}>2023-08월</Text>
-        </View>
+        <Pressable onPress={() => setShow(prev => !prev)}>
+          <Text style={styles.dateText}>2023년 8월</Text>
+        </Pressable>
         <View style={styles.diaryListView}>
           <FlatList
             ref={flatListRef}
@@ -83,6 +99,19 @@ const Diary = () => {
           />
         </View>
         <DiaryWriteButton />
+        {show && (
+          <MonthPicker
+            autoTheme={false}
+            // onChange={onValueChange}
+            value={date}
+            minimumDate={new Date()}
+            maximumDate={new Date(2025, 5)}
+            locale="ko"
+            okButton="선택"
+            cancelButton="취소"
+            mode="short"
+          />
+        )}
       </View>
     </SafeAreaView>
   );
