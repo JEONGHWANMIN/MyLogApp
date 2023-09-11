5f8c0a9c532d7c820b80de19cfc6817f1406a995
JEONGHWANMIN <mimi0j0j@naver.com>
2023-09-11 23:44:51 +0900

    Feat : 캘린더 디펜던시 추가 및 기본 컴포넌트 세팅

--- app/src/screens/BottomTab/Home.tsx
+++ app/src/screens/BottomTab/Home.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import {theme} from '@/styles/theme';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {Calendar} from 'react-native-calendars';
+import {Text} from 'react-native-paper';
 
 const Home = () => {
+  const [selected, setSelected] = useState('');
+  const [currentDate, setCurrentDate] = useState({});
   return (
-    <View>
-      <Text>홈 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Calendar
+          onDayPress={day => {
+            setSelected(day.dateString);
+          }}
+          onMonthChange={month => console.log(month)}
+          monthFormat="yyyy년 MM월"
+          markedDates={{
+            [selected]: {
+              selected: true,
+              disableTouchEvent: true,
+              selectedColor: theme.colors.point.sageGreen,
+            },
+            '2023-09-01': {selected: true, marked: true, selectedColor: 'blue'},
+            '2023-09-02': {marked: true},
+            '2023-09-03': {
+              selected: true,
+              marked: true,
+              selectedColor: theme.colors.point.mintGreen,
+            },
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  calendar: {},
+});
