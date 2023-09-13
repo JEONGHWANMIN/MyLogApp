309272caa0d0c82970ae31e80a525ddf55ab8f77
JEONGHWANMIN <mimi0j0j@naver.com>
2023-09-13 23:58:20 +0900

    Feat : 일기 요약 API 일기 작성 날짜 추가

--- app/src/orval/model/diaryApiSpecGetDiarySummary200Data.ts
+++ app/src/orval/model/diaryApiSpecGetDiarySummary200Data.ts
@@ -7,6 +7,7 @@
 import type {KeyStringValueString} from './keyStringValueString';
 
 export type DiaryApiSpecGetDiarySummary200Data = {
+  userDiaryDateList: string[];
   totalDiariesCount: number;
   monthDiariesCount: number;
   moodCountList: KeyStringValueString[];
