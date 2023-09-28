3f1bb32f59e426a387a0517f217c8a2f2c56d74e
dev-andrew97 <andrew@kimzipsa.co.kr>
2023-09-28 16:10:11 +0900

    Feat : 일기 요약 리패칭 추가

--- src/screens/DiaryDetail/_query/usePatchDiaryById.ts
+++ src/screens/DiaryDetail/_query/usePatchDiaryById.ts
@@ -3,6 +3,7 @@ import {useDiaryApiSpecPatchDiaryId} from '@/orval/api/diary/diary';
 import {DiaryApiSpecPatchDiaryIdBody} from '@/orval/model';
 import {useGlobalDialogStore} from '@/utils/state/dialog.zustand';
 import {useRefetchDiaryList} from './useRefetchDiaryList';
+import {useRefetchDiarySummary} from '@/screens/Write/_hooks/useRefetchDiarySummary';
 
 interface PatchDiaryById {
   refetchDiaryById: () => void;
@@ -13,6 +14,7 @@ const usePatchDiaryById = ({refetchDiaryById, handleEditMode}: PatchDiaryById) =
   const {setGlobalDialogConfig} = useGlobalDialogStore();
   const {showSnackbarMessage} = useShowSnackbarMessage();
   const {refetchDiaryList} = useRefetchDiaryList();
+  const {refetchDiarySummary} = useRefetchDiarySummary();
 
   const patchDiaryMutate = useDiaryApiSpecPatchDiaryId();
 
@@ -25,9 +27,10 @@ const usePatchDiaryById = ({refetchDiaryById, handleEditMode}: PatchDiaryById) =
       {
         onSuccess: () => {
           refetchDiaryById();
+          refetchDiaryList();
+          refetchDiarySummary();
           handleEditMode();
           showSnackbarMessage('일기 수정이 완료되었습니다.', 'info');
-          refetchDiaryList();
         },
         onError: () => {
           showSnackbarMessage('일기 수정이 실패했습니다.', 'error');
