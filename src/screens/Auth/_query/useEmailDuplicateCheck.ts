449d1422af083366e715306bec409f384a08b003
JEONGHWANMIN <mimi0j0j@naver.com>
2023-10-08 04:25:37 +0900

    Feat : 이메일 형식 체크 추가

--- src/screens/Auth/_query/useEmailDuplicateCheck.ts
+++ src/screens/Auth/_query/useEmailDuplicateCheck.ts
@@ -1,5 +1,6 @@
 import {useShowSnackbarMessage} from '@/hooks/useShowSnacbarMessage';
 import {useUsersApiSpecPostUsersCheck} from '@/orval/api/users/users';
+import {Validation} from '@/utils/util/Validation';
 
 interface EmailDuplicateCheck {
   handleSetEmailDuplicate: (isDuplicate: boolean) => void;
@@ -11,6 +12,15 @@ const useEmailDuplicateCheck = ({handleSetEmailDuplicate}: EmailDuplicateCheck) 
   const userEmailDuplicateCheckMutate = useUsersApiSpecPostUsersCheck();
 
   const handleEmailDuplicate = async (email: string) => {
+    if (!email) {
+      showSnackbarMessage('유효하지 않은 이메일 입니다.', 'warning');
+      return;
+    }
+
+    if (!Validation.isEmailValid(email)) {
+      showSnackbarMessage('유효하지 않은 이메일 입니다.', 'warning');
+      return;
+    }
     userEmailDuplicateCheckMutate.mutate(
       {
         data: {
---
+++ src/utils/util/Validation.ts
@@ -0,0 +1,7 @@
+export class Validation {
+  static isEmailValid(email: string) {
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+    return emailRegex.test(email);
+  }
+}
